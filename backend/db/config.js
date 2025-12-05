a88936c499342a4c8bd07c0dba4e76712b8cffd7
Ayaan <halaiayaan568@gmail.com>
2025-12-05 20:21:53 +0530

    shift mongodb to online server

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,2 +1,4 @@
+require("dotenv").config()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bookstore')
+// mongoose.connect(process.env.DB_URL_LOCAL)
+mongoose.connect(process.env.DB_URL)
